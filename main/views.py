e26451ccfd91b38a9073ecb910077e9159cad1de
Diaz Prayodhi Iskandar <diaz.prayodhi@cs.ui.ac.id>
2025-09-10 09:50:00 +0700

    terakhir

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'Aplikasi' : 'tokobola',
+        'aplikasi' : 'tokobola',
         'nama': 'Diaz Prayodhi Iskandar',
         'class': 'PBP D'
     }
