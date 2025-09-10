25314fd1063dd677095d7d71ca91a721d0afae95
Diaz Prayodhi Iskandar <diaz.prayodhi@cs.ui.ac.id>
2025-09-10 08:52:43 +0700

    commit

---
+++ main/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApptoboConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apptobo'
---
+++ main/models.py
@@ -0,0 +1,35 @@
+import uuid
+from django.db import models
+
+class Item(models.Model):
+    CATEGORY_CHOICES = [
+        ('sportswear', 'Sportswear'),
+        ('bola', 'Bola'),
+        ('booster', 'Booster'),
+        ('headband', 'Headband'),
+        ('others', 'Others'),
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)  # nama item
+    price = models.IntegerField()            # harga item
+    description = models.TextField()         # deskripsi item
+    thumbnail = models.URLField(blank=True, null=True)  # gambar item
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='others')
+    is_featured = models.BooleanField(default=False)    
+    
+    # contoh atribut tambahan
+    stock = models.PositiveIntegerField(default=0)      # stok item
+    rating = models.FloatField(default=0.0)             # rating item
+    
+    def __str__(self):
+        return self.name
+
+    
+    @property
+    def is_news_hot(self):
+        return self.news_views > 20
+        
+    def increment_views(self):
+        self.news_views += 1
+        self.save()
