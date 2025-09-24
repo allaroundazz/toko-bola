6fb0803f334b82b921e05dece794b139763b0607
Diaz Prayodhi Iskandar <diaz.prayodhi@cs.ui.ac.id>
2025-09-24 11:04:35 +0700

    test

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 class Item(models.Model):
     CATEGORY_CHOICES = [
@@ -17,6 +18,7 @@ class Item(models.Model):
     thumbnail = models.URLField(blank=True, null=True)  # gambar item
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='others')
     is_featured = models.BooleanField(default=False)    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     
     # contoh atribut tambahan
     stock = models.PositiveIntegerField(default=0)      # stok item
